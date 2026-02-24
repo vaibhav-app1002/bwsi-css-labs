a94126b2a650beeefab49281da34e00203a848bc
V-ibes <166233801+V-ibes@users.noreply.github.com>
2026-02-23 19:51:17 -0500

    add user input sanitization

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -9,6 +9,9 @@ and prints the result to the terminal window.
 
 """
 
+from ast import While
+
+
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
     Function that takes in two numbers and an operation (add, subtract, multiply, divide),
@@ -37,13 +40,23 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def request_sanitized_number(prompt: str) -> int:
+    """
+    Function that prompts the user for a number and ensures that the input is a valid float.
+    """
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = request_sanitized_number("Enter the first number: ")
+    num2 = request_sanitized_number("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
